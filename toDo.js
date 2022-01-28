3bc3717048f018ce9234f5d711a982063aba62cd
MariaR <rudneva@it-tian.ru>
2022-01-28 10:52:23 +0500

    create todo task

---
+++ toDo.js
@@ -0,0 +1,59 @@
+const list = {
+  "create a new practice task": "In Progress",
+  "make a bed": "Done",
+  "write a post": "To Do",
+};
+let toDoTasks = "";
+let inProgressTasks = "";
+let doneTasks = "";
+
+function changeStatus(task, status) {
+  if ([task] in list) {
+    list[task] = status;
+  }
+}
+
+function addTask(task) {
+  list[task] = "To Do";
+}
+
+function deleteTask(task) {
+  delete list[task];
+}
+
+function showList(obj) {
+  for (let task in obj) {
+    if (obj[task] == "To Do") {
+      toDoTasks += `\n \"${[task]}"`;
+    } else if (obj[task] == "In Progress") {
+      inProgressTasks += `\n \"${[task]}"`;
+    } else {
+      doneTasks += `\n \"${[task]}"`;
+    }
+  }
+  if (toDoTasks.length === 0) {
+    toDoTasks += "\n -";
+  }
+  if (inProgressTasks.length === 0) {
+    inProgressTasks += "\n -";
+  }
+  if (doneTasks.length === 0) {
+    doneTasks += "\n -";
+  }
+  return `Todo: ${toDoTasks} \nIn Progress: ${inProgressTasks} \nDone: ${doneTasks}`;
+}
+
+changeStatus("create a new practice task", "Done");
+addTask("have a walk");
+addTask("make toDo list");
+addTask("finish the work week");
+addTask("pass the algorithms exam");
+changeStatus("make toDo list", "Done");
+changeStatus("pass the algorithms exam", "In Progress");
+changeStatus("finish the work week", "In Progress");
+deleteTask("have a walk");
+deleteTask("make a bed");
+deleteTask("create a new practice task");
+deleteTask("write a post");
+
+console.log(showList(list));
