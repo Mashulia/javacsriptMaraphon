314c7be68588e9dc03b83f844ee41965020b4a5b
MariaR <rudneva@it-tian.ru>
2022-01-31 17:25:51 +0500

    refactored

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,9 +1,12 @@
-for (let i = 1; i < 100; i++) {
+const startNumber = 5;
+const endNumber = 100;
+
+for (let i = startNumber; i < endNumber; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
     console.log("FizzBuzz");
-  } else if (i % 3 === 0 && i % 5 !== 0) {
+  } else if (i % 3 === 0) {
     console.log("Fizz");
-  } else if (i % 5 === 0 && i % 3 !== 0) {
+  } else if (i % 5 === 0) {
     console.log("Buzz");
   } else {
     console.log(i);
